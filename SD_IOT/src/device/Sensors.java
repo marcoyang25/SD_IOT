3a946a846333361aff1658e05db7a5eb86a6bad4
marco <s105062638@m105.nthu.edu.tw>
2017-07-15 17:15:55 +0800

    Move initialization of Sensor into loop to improve readability

--- SD_IOT/src/device/Sensors.java
+++ SD_IOT/src/device/Sensors.java
@@ -4,17 +4,15 @@ import java.util.*;
 
 public class Sensors extends HashMap<Integer, Sensor> {
 	public Sensors(int n) {
-		Sensor s;
 		for (int i = 0; i < n; i++) {
-			s = new Sensor();
+			Sensor s = new Sensor();
 			this.put(s.getId(), s);
 		}
 	}
 	
 	public Sensors(int n, Targets targets) {
-		Sensor s;
 		for (int i = 0; i < n; i++) {
-			s = new Sensor(targets);
+			Sensor s = new Sensor(targets);
 			this.put(s.getId(), s);
 		}
 	}
