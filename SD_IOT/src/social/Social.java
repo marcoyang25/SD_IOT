df3b133eebad518252e7731bb3e8edecc683653e
marco <s105062638@m105.nthu.edu.tw>
2017-07-18 19:49:53 +0800

    Added createIntGraph

--- SD_IOT/src/social/Social.java
+++ SD_IOT/src/social/Social.java
@@ -1,7 +1,11 @@
 package social;
 
-import java.util.Set;
-import org.jgrapht.graph.DefaultEdge;
+import java.util.*;
+import org.jgraph.*;
+import org.jgrapht.generate.*;
+import org.jgrapht.graph.*;
+import org.jgrapht.Graph;
+import org.jgrapht.VertexFactory;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import device.Sensor;
 
@@ -26,5 +30,26 @@ public class Social {
 		}
 		return false;
 	}
+
+	// create a complete graph
+	public static Graph<Integer, DefaultEdge> createIntGraph(List<Sensor> sensors) {
+		Graph<Integer, DefaultEdge> g = new SimpleGraph<>(DefaultEdge.class);
+		// Create the CompleteGraphGenerator object
+		CompleteGraphGenerator<Integer, DefaultEdge> completeGenerator = new CompleteGraphGenerator<>(sensors.size());
+		// Create the VertexFactory so the generator can create vertices
+		VertexFactory<Integer> vFactory = new VertexFactory<Integer>() {
+			private int index = 0;
+
+			@Override
+			public Integer createVertex() {
+				return sensors.get(index++).getId();
+			}
+		};
+
+		// Use the CompleteGraphGenerator object to make completeGraph a
+		// complete graph with [sensors.size()] number of vertices
+		completeGenerator.generateGraph(g, vFactory, null);
+		return g;
+	} // end method createIntGraph
 
 }
