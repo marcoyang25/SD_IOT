96c023085a6601317c6860551fc8fd0ff252ab8e
marco <s105062638@m105.nthu.edu.tw>
2017-07-19 16:06:09 +0800

    Added method generateRandomRequests

--- SD_IOT/src/device/Targets.java
+++ SD_IOT/src/device/Targets.java
@@ -11,8 +11,8 @@ public class Targets extends HashMap<Integer, Target> {
 	}
 
 	/**
-	 * return true if each target is covered by at least one sensor, return
-	 * false otherwise
+	 * Return true if each target is covered by at least one sensor, return
+	 * false otherwise.
 	 */
 	public static boolean hasSensorCover(Targets targets) {
 		for (Target target : targets.values()) {
@@ -22,6 +22,26 @@ public class Targets extends HashMap<Integer, Target> {
 		}
 		return true;
 	}
+
+	/**
+	 * Randomly select requests that request this target, and at least one
+	 * request will be selected.
+	 */
+	public static void generateRandomRequests(Targets targets, Requests requests) {
+		final Random random = new Random();
+		for (Target target : targets.values()) {
+			int randomRequestNumber = random.nextInt(1) + 1;
+			List<Request> values = new ArrayList<>(requests.values());
+			for (int i = 0; i < randomRequestNumber; i++) {
+				Request randomRequest = values.get(random.nextInt(values.size()));
+				randomRequest.AddLocation(target);
+				target.addRequestedBy(randomRequest);
+				// remove this request so that the target will not
+				// select it again
+				values.remove(randomRequest);
+			}
+		}
+	} // end method generateRandomRequests
 
 	@Override
 	public String toString() {
