cd5d516f8236e75a144acf6e1797f290ee8ce3dc
marco <s105062638@m105.nthu.edu.tw>
2017-07-20 14:06:12 +0800

    Added generateVirtualTargets

--- SD_IOT/src/device/Requests.java
+++ SD_IOT/src/device/Requests.java
@@ -1,12 +1,23 @@
 package device;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Requests extends HashMap<Integer, Request> {
 	public Requests(int n) {
 		for (int i = 0; i < n; i++) {
 			Request r = new Request();
 			this.put(r.getId(), r);
+		}
+	}
+
+	public static void generateVirtualTargets(Requests requests) {
+		for (Request request : requests.values()) {
+			Set<VirtualTarget> virtualTargets = new HashSet<>();
+			for (Target target : request.getLocations()) {
+				VirtualTarget v = new VirtualTarget(target.getId(), request.getId());
+				virtualTargets.add(v);
+			}
+			request.setLocations(virtualTargets);
 		}
 	}
 
