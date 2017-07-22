42036efa4a6c9e16c107862c77cddc7cdf955075
marco <s105062638@m105.nthu.edu.tw>
2017-07-22 18:17:38 +0800

    elect 1 ~ request_num requests

--- SD_IOT/src/device/Targets.java
+++ SD_IOT/src/device/Targets.java
@@ -24,15 +24,19 @@ public class Targets extends HashMap<Integer, Target> {
 	}
 
 	/**
-	 * Randomly select requests that request this target, and at least one
-	 * request will be selected.
+	 * Randomly select 1 ~ request_num requests that request this target, and at
+	 * least one request will be selected.
 	 */
-	public static void generateRandomRequests(Targets targets, Requests requests) {
+	public static void generateRandomRequests(Targets targets, Requests requests, int requests_num) {
 		final Random random = new Random();
+		if (requests_num > requests.size()) {
+			System.err.println("request_num is larger than requests.size()");
+			return;
+		}
 		for (Target target : targets.values()) {
-			int randomRequestNumber = random.nextInt(1) + 1;
+			int randomRequestsNumber = random.nextInt(requests_num) + 1;
 			List<Request> values = new ArrayList<>(requests.values());
-			for (int i = 0; i < randomRequestNumber; i++) {
+			for (int i = 0; i < randomRequestsNumber; i++) {
 				Request randomRequest = values.get(random.nextInt(values.size()));
 				randomRequest.AddLocation(target);
 				target.addRequestedBy(randomRequest);
